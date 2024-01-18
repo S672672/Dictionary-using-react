71b336c714652f67e9daa962aad469bfb6256570
Master <someone@gmail.com>
2024-01-18 21:40:03 +0530

    design completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useEffect } from 'react'
 import './App.css'
+import { useState } from 'react'
+import axios from 'axios'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
+  const[words,setWord] = useState('')
+  useEffect(()=>{
+    const response = axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/<word>`)
+    setWord(response.data);
+    console.log(response.data)
+  })
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='wholebody'>
+    <h1 className='text-4xl font-bold m-5'>Dictionary App</h1>
+      <div className='content'>
+      <input className='input' placeholder='Search here'></input>
+      <button className='button'>Search</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
-export default App
