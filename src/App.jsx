e55f3ef93bbce5a83e5304ebb2abbfcca6eada49
Master <someone@gmail.com>
2024-01-20 14:13:24 +0530

    design

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,43 @@
-import React, { useEffect } from 'react'
-import './App.css'
-import { useState } from 'react'
-import axios from 'axios'
+import React, { useEffect } from "react";
+import "./App.css";
+import { useState } from "react";
+import axios from "axios";
 
 export default function App() {
-  const[words,setWord] = useState('')
-  useEffect(()=>{
-    const response = axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/<word>`)
-    setWord(response.data);
-    console.log(response.data)
-  })
+  const [keyWord, setKeyWord] = useState("");
+
+  const [result, setResult] = useState(null);
+
+  const api = "https://api.dictionaryapi.dev/api/v2/entries/en";
+
+  async function handleSearch() {
+    try {
+      const res = await axios.get(`${api}/${keyWord}`);
+      console.log(res, "res");
+      setResult(res.data[0]);
+    } catch (e) {
+      console.log({ e });
+    }
+  }
+
+  function handleClear() {
+    setKeyWord("");
+    setResult(null);
+  }
   return (
-    <div className='wholebody'>
-    <h1 className='text-4xl font-bold m-5'>Dictionary App</h1>
-      <div className='content'>
-      <input className='input' placeholder='Search here'></input>
-      <button className='button'>Search</button>
+    <div className="wholebody">
+      <h1 className="text-4xl font-bold m-5">Dictionary App</h1>
+      <div className="content">
+        <input
+          className="input"
+          placeholder="Search here"
+          onChange={(e) => setKeyWord(e.target.value)}
+          value={keyWord}
+        ></input>
+        <button className="button" type='submit' onClick={handleSearch}>Search</button>
+        
+
       </div>
     </div>
-  )
+  );
 }
-
